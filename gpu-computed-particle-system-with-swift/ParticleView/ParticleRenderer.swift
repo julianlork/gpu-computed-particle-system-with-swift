9bd450a46c2a5ea17b79bec16c564549a946d6d6
Julian Lork <118125250+julianlork@users.noreply.github.com>
2023-09-04 17:36:29 +0200

    added-basic-structure-of-renderer

---
+++ gpu-computed-particle-system-with-swift/ParticleView/ParticleRenderer.swift
@@ -0,0 +1,29 @@
+//
+//  ParticleRenderer.swift
+//  gpu-computed-particle-system-with-swift
+//
+//  Created by Julian Lork on 04.09.23.
+//
+
+import Foundation
+import MetalKit
+
+class ParticleRenderer: NSObject {
+    
+    let view: MTKView = MTKView()
+    
+    
+}
+
+extension ParticleRenderer: MTKViewDelegate {
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        
+    }
+    
+    
+}
