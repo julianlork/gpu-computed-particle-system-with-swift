e7720e71d26a565c6e0910673492d07be583b788
Julian Lork <118125250+julianlork@users.noreply.github.com>
2023-09-04 17:25:41 +0200

    Initial Commit

---
+++ gpu-computed-particle-system-with-swift/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  gpu-computed-particle-system-with-swift
+//
+//  Created by Julian Lork on 04.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gpu-computed-particle-system-with-swift/gpu_computed_particle_system_with_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpu_computed_particle_system_with_swiftApp.swift
+//  gpu-computed-particle-system-with-swift
+//
+//  Created by Julian Lork on 04.09.23.
+//
+
+import SwiftUI
+
+@main
+struct gpu_computed_particle_system_with_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
