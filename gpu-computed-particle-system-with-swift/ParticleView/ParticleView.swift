dc3f72cc774a7e2558d4145244c3f3220bc84e88
Julian Lork <118125250+julianlork@users.noreply.github.com>
2023-09-04 17:36:05 +0200

    initial-commit-of-particle-view

---
+++ gpu-computed-particle-system-with-swift/ParticleView/ParticleView.swift
@@ -0,0 +1,58 @@
+//
+//  ParticleView.swift
+//  gpu-computed-particle-system-with-swift
+//
+//  Created by Julian Lork on 04.09.23.
+//
+
+import SwiftUI
+import MetalKit
+
+struct ParticleView: View {
+    var body: some View {
+        ParticleSystem()
+    }
+}
+
+struct ParticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParticleView()
+    }
+}
+
+#if os(macOS)
+typealias ViewRepresentable = NSViewRepresentable
+#elseif os(iOS)
+typealias ViewRepresentable = UIViewRepresentable
+#endif
+
+struct ParticleSystem: ViewRepresentable {
+    
+    #if os(macOS)
+    func makeNSView(context: Context) -> some NSView {
+        return context.coordinator.view
+    }
+    
+    func updateNSView(_ nsView: NSViewType, context: Context) {
+        updateView(context)
+    }
+    
+    #elseif os(iOS)
+    func makeUIView(context: Context) -> some UIView {
+        return context.coordinator.view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        updateView(context)
+    }
+    
+    #endif
+    
+    func updateView(_ context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> ParticleRenderer {
+        return ParticleRenderer()
+    }
+}
