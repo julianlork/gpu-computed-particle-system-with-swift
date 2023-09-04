66f7e6ee966aaf85df540591cd7e5eea3b7186c9
Julian Lork <118125250+julianlork@users.noreply.github.com>
2023-09-04 18:01:18 +0200

    included-particle-definition-to-bridging-header

---
+++ gpu-computed-particle-system-with-swift/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  gpu-computed-particle-system-with-swift
+//
+//  Created by Julian Lork on 04.09.23.
+//
+
+#ifndef Common_h
+#define Common_h
+
+#include "ParticleView/ParticleDefinition.h"
+
+#endif /* Common_h */
