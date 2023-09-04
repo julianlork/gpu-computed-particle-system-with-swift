3c242aefcaf212f9871f785773a0691636771bfb
Julian Lork <118125250+julianlork@users.noreply.github.com>
2023-09-04 18:02:07 +0200

    added-renderer-initialization

--- gpu-computed-particle-system-with-swift/ParticleView/ParticleRenderer.swift
+++ gpu-computed-particle-system-with-swift/ParticleView/ParticleRenderer.swift
@@ -11,8 +11,72 @@ import MetalKit
 class ParticleRenderer: NSObject {
     
     let view: MTKView = MTKView()
+    let device: MTLDevice
+    let commandQueue: MTLCommandQueue
+    let clearPass: MTLComputePipelineState
+    let drawDotPass: MTLComputePipelineState
+    let particleBuffer: MTLBuffer
+    
+    init(_ numParticle: UInt) {
+        
+        let particles = ParticleRenderer.createRandomParticles(numParticle)
+        
+        guard
+            let device = MTLCreateSystemDefaultDevice(),
+            let commandQueue = device.makeCommandQueue(),
+            let particleBuffer = device.makeBuffer(bytes: particles, length: MemoryLayout<Particle>.stride * particles.count, options: .storageModeShared),
+            let library = device.makeDefaultLibrary(),
+            let clearPass = ParticleRenderer.makeClearPassFcn(device, library),
+            let drawPass = ParticleRenderer.makeDrawPassFcn(device, library) else { fatalError("Particle Renderer initialization failed.") }
+        
+        self.device = device
+        self.commandQueue = commandQueue
+        self.particleBuffer = particleBuffer
+        self.clearPass = clearPass
+        self.drawDotPass = drawPass
+        super.init()
+
+        self.view.device = device
+        self.view.framebufferOnly = false
+        self.view.delegate = self
+    }
     
     
+    class func makeDrawPassFcn(_ device: MTLDevice, _ library: MTLLibrary) -> MTLComputePipelineState? {
+        guard let drawPassFcn = library.makeFunction(name: "drawPassFcn") else {
+            fatalError("Library could not find the function 'drawPassFcn'.")
+        }
+        
+        do {
+            return try device.makeComputePipelineState(function: drawPassFcn)
+        } catch {
+            return nil
+        }
+    }
+    
+    class func makeClearPassFcn(_ device: MTLDevice, _ library: MTLLibrary) -> MTLComputePipelineState? {
+        guard let clearPassFcn = library.makeFunction(name: "clearPassFcn") else {
+            fatalError("Library could not find the function 'clearPassFcn'.")
+        }
+        
+        do {
+            return try device.makeComputePipelineState(function: clearPassFcn)
+        } catch {
+            return nil
+        }
+    }
+    
+    class func createRandomParticles(_ numParticle: UInt) -> [Particle] {
+        var particles: [Particle] = []
+        
+        for _ in 0...numParticle {
+            let particle = Particle(color: vector_float4(1, 1, 1, 1),
+                                    position: vector_float2(Float.random(in: 0...1), Float.random(in: 0...1)),  /// screen normalized coordinates where x=1 equals max width  and y=1 max height
+                                    velocity: vector_float2(Float.random(in: -1...1), Float.random(in: -1...1)))
+            particles.append(particle)
+        }
+        return particles
+    }
 }
 
 extension ParticleRenderer: MTKViewDelegate {
